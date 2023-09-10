2548ecf873ab92f2adfc8f0bb8c87306c053dee5
Tedros Alem <tedsaasfaha@gmail.com>
2023-09-10 11:35:26 +0300

    motion from framer imported to Header component

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
 import './Header.scss';
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
 
 const Header = () => {
   return (
