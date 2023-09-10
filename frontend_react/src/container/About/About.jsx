10ef0972e9de0f854c1f6d4ca79154f684ac3572
Tedros Alem <tedsaasfaha@gmail.com>
2023-09-10 16:11:10 +0300

    About.jsx file implemented

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
 import './About.scss';
+
+const abouts = [
+  { title: 'FrontEnd Development', description: 'I am a good frontend developer', imgUrl: images.about01 },
+  { title: 'BackEnd Developer', description: 'I am a good backend developer', imgUrl: images.about02 },
+  { title: 'FullStack Developer', description: 'I am a good fullstack developer', imgUrl: images.about03 },
+  { title: 'Web Developer', description: 'I am a good frontend developer', imgUrl: images.about04 }
+];
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className='head-text' >
+      I know that
+      <span>Good Design</span>
+      <br />
+      means
+      <span>good Business</span>
+      </h2>
+
+      <div className='app__profiles' >
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className='app__profile-item'
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }} >{about.title}</h2>
+            <p className='p-text' style={{ marginTop: 10 }} >{about.description}</p>
+
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 }
 
